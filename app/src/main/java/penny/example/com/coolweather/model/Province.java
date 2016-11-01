2cc3f270b6693e7ad5a312bcad1f8c06af3b394f
penny_xiaojuan <penny_xiaojuan@outlook.com>
2016-11-01 16:09:38 +0800

    新增数据库帮助类

---
+++ app/src/main/java/penny/example/com/coolweather/model/Province.java
@@ -0,0 +1,30 @@
+package penny.example.com.coolweather.model;
+
+/**
+ * Created by penny on 2016/11/1.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void getProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+    public String getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(String provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
